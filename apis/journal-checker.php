1d55a88d910854210337b1751003fd77b665b6e5
Rochmady <srochmady@gmail.com>
2026-06-25 17:29:44 +0700

    Update wide size results container

--- apis/journal-checker.php
+++ apis/journal-checker.php
@@ -935,7 +935,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['issn'])) {
         }
         
         .navbar-container {
-            max-width: 1280px;
+            max-width: 1180px;
             margin: 0 auto;
             display: flex;
             justify-content: space-between;
@@ -1030,7 +1030,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['issn'])) {
            MAIN CONTAINER
            ================================================================== */
         .container {
-            max-width: 1280px;
+            max-width: 1080px;
             margin: 70px auto;
             /*background: white;*/
             border-radius: var(--radius);
@@ -2031,7 +2031,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['issn'])) {
         }
         
         .footer-container {
-            max-width: 1280px;
+            max-width: 1180px;
             margin: 0 auto;
             padding: 0 20px;
         }
