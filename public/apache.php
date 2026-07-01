9144097b985ba2a4f4f308d00b51a777dab97fb7
Rochmady <srochmady@gmail.com>
2026-07-02 00:16:45 +0700

    Enhance documentation and add strict types declaration in apache.php

--- public/apache.php
+++ public/apache.php
@@ -1,4 +1,19 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * @file public/apache.php
+ *
+ * Copyright (c) 2024-2026 Sangia Lumera Publishing
+ * Copyright (c) 2017-2026 Rochmady and Code Lumera Teams
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class ApacheCheck
+ * @ingroup apis
+ *
+ * @brief Handle requests for Apache-specific information.
+ */
+
 echo "<h3>Server Information</h3>";
 echo "Server Software: " . $_SERVER['SERVER_SOFTWARE'] . "<br>";
 echo "PHP Version: " . PHP_VERSION . "<br>";
